2c507f2245333f7f0e01c47c967ebf82263addfa
Aldiyar <adelov.aldiyar@gmail.com>
2022-04-07 12:41:02 +0600

    Lab4

--- Lab4/ToDo/app/src/main/java/com/example/todo/MainActivity.kt
+++ Lab4/ToDo/app/src/main/java/com/example/todo/MainActivity.kt
@@ -2,13 +2,9 @@ package com.example.todo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.room.Room
 import com.example.todo.dao.TodoDao
 import com.example.todo.databinding.ActivityMainBinding
-import com.example.todo.models.Category
-import com.example.todo.models.ToDo
-import com.example.todolist.MainApplication
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
